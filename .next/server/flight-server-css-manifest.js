27fbbd8ff7beb47e4b5b5843a449ad35c5d9a384
Anas0055 <manaskhan969@gmail.com>
2024-05-03 21:27:49 -0700

    resume

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "C:\\Users\\Hp\\Desktop\\resume-app\\app\\page": [
+      "C:\\Users\\Hp\\Desktop\\resume-app\\app\\global.css"
+    ]
+  },
+  "C:\\Users\\Hp\\Desktop\\resume-app\\app\\layout.tsx": [
+    "C:\\Users\\Hp\\Desktop\\resume-app\\app\\global.css"
+  ]
+}
---
+++ data/page-data.jsx
@@ -0,0 +1,66 @@
+export const personalData = {
+	name: "Muhammad Anas",
+	role: "Software Engineer",
+	education: [
+		"Fsc form govt college charsadda",
+		"Student of PIAIC, cecos university  peshawaer ",
+	],
+	contactLinks: [
+		"mailto:emailto:manaskhan969@gmail.com",
+		"https://twitter.com/",
+		"https://linkedin.com/in/anaskhanturangzi",
+        "https://www.instagram.com/muhammadanas4109?igsh=YWlhdW5saHZ1MzIy&utm_source=qr",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		"As a highly motivated and driven individual with a passion for making a positive impact, I have a unique blend of technical and interpersonal skills, thanks to my background in computer science. I am able to tackle complex problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
+		"When I'm not working, I love hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+		{ icon: "💻", text: "Python" },
+		{ icon: "ʦ", text: "TypeScript" },
+		{ icon: "🚀", text: "React" },
+		{ icon: "💾", text: "Html" },
+		{ icon: "💾", text: "Css" },
+		{ icon: "📈", text: "Data Structures and Algorithms" },
+	],
+};
+
+export const professionalData = {
+	title: "Languages",
+	experiences: [
+		{
+			role: "Poshto",
+			description:
+				"Native",
+			current: true,
+		},
+
+		{
+			role: "Urdu",
+			description:
+				"Professional",
+			current: true,
+		},
+		{
+			role: "English",
+			description:
+				"Little Bit",
+			current: true,
+		},
+	],
+};
